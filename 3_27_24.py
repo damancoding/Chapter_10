f03a094d597c2308212f9d5bb6e89e34e19028c0
Amanda M <156499790+damancoding@users.noreply.github.com>
2024-03-27 20:53:50 -0400

    Add files via upload
    These are the notes that were taken

---
+++ 3_27_24.py
@@ -0,0 +1,23 @@
+#Research how to make a database
+#Look up how to make a music database (THE HARDEST THING TO DO ATP)  
+# learning abotu different data structures but arrays are going to be consistent
+# A file should be able to Open, be Read, and Close.
+# Types of files included in text and binary
+    #- text file: data encoded as text using ASCII ro UNIcode.
+    # - binary file: data that hasn't been converted to text yet (need a text editor)
+
+#Delimiters:  a predefined character or set of characters that mark the end of each piece of data
+    #- It separates the different items within the files( if you get good at noticing and working w/ them, you can pull them like strings to get what you want)
+# EOF or End of File markers: a special character or set of characters written to the end of the file ( used to be phrased as "\end")
+
+#A record is a complete set of data abotu an item (like the list in a folder)
+
+## Catch up on Chapter 6 to Chapter 10 (Turn in everything the way he wants it: flowchart, PseudoCode, Actual Code)
+
+# Review Chapter 11 (We'll be going over that when we come back)
+# Then Chapter 15 by the week after that
+# All work needs to be done by the 24th and final projects
+# Study for the Finals
+#------------------------------------------------------
+from phone_book import index
+
